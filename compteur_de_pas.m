79b925383f03169dd23860fe79beebab0f7c82ca
Horia <horia.petre@outlook.com>
2020-02-16 13:58:59 -0500

    - able to read csv file in matlab 2017 - created lab2_part1_testdata1_mod.csv so that matlab can read values

--- compteur_de_pas.m
+++ compteur_de_pas.m
@@ -6,11 +6,11 @@
 % time, x, y, z matrices
 clear all
 close all
-[num, txt, raw] = xlsread('LAB2_Part1_testdata1.csv');
-time_matrix     = str2double(txt(2:end, 1));
-x_matrix        = str2double(txt(2:end, 2));
-y_matrix        = str2double(txt(2:end, 3));
-z_matrix        = str2double(txt(2:end, 4));
+[num, txt, raw] = xlsread('LAB2_Part1_testdata1_mod.csv');
+time_matrix     = num(1:end, 1);
+x_matrix        = num(1:end, 2);
+y_matrix        = num(1:end, 3);
+z_matrix        = num(1:end, 4);
 % plot
 % X
 figure
@@ -27,13 +27,20 @@ xlabel('Time [s]');
 ylabel('Acceleration [m/s^2]');
 grid on
 % Z
-z_matrix_no_g = z_matrix - nanmean(z_matrix);
+z_matrix_no_g = z_matrix - nanmean(z_matrix) % substract gravity from acc;
+minPeakHeight = nanstd(z_matrix_no_g);
+[pks,locs] = findpeaks(z_matrix_no_g,'MINPEAKHEIGHT',minPeakHeight);
+numSteps = numel(pks);
+
 figure
 plot(time_matrix, z_matrix_no_g);
-title('Z vs Time')
+title("Z acceleration without gravity vs Time, Number of steps = " + numSteps)
 xlabel('Time [s]');
 ylabel('Acceleration [m/s^2]');
 grid on
+hold on
+plot(time_matrix(locs), pks, 'r', 'Marker', 'v', 'LineStyle', 'none');
+
 % Acceleration Magnitude
 acc_mag = sqrt(sum(x_matrix.^2 + y_matrix.^2 + z_matrix_no_g.^2,2));
 figure
@@ -42,15 +49,5 @@ title('Total Acceleration vs Time')
 xlabel('Time [s]');
 ylabel('Acceleration [m/s^2]');
 grid on
-
-minPeakHeight = nanstd(acc_mag);
-[pks,locs] = findpeaks(acc_mag,'MINPEAKHEIGHT',minPeakHeight);
-numSteps = numel(pks);
-% hold on
-% plot(time_matrix(locs), pks, 'r', 'Marker', 'v', 'LineStyle', 'none');
-% title('Counting Steps');
-% xlabel('Time (s)');
-% ylabel('Acceleration Magnitude, No Gravity (m/s^2)');
-% hold off
 
 
