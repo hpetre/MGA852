a54d3f733382fc94ef96ac684a272a04f8884477
Horia <horia.petre@outlook.com>
2020-02-16 14:56:03 -0500

    plotted number of steps as a function of peak size

--- compteur_de_pas.m
+++ compteur_de_pas.m
@@ -6,6 +6,7 @@
 % time, x, y, z matrices
 clear all
 close all
+clc
 [num, txt, raw] = xlsread('LAB2_Part1_testdata1_mod.csv');
 time_matrix     = num(1:end, 1);
 x_matrix        = num(1:end, 2);
@@ -27,18 +28,31 @@ xlabel('Time [s]');
 ylabel('Acceleration [m/s^2]');
 grid on
 % Z
-z_matrix_no_g = z_matrix - nanmean(z_matrix) % substract gravity from acc;
-minPeakHeight = nanstd(z_matrix_no_g);
-[pks,locs] = findpeaks(z_matrix_no_g,'MINPEAKHEIGHT',minPeakHeight);
-numSteps = numel(pks);
+z_matrix_no_g = z_matrix - nanmean(z_matrix); % substract gravity from acc
+
+indx = 1;
+for a=0.0:0.1:4
+    minPeakHeight(indx) = a * nanstd(z_matrix_no_g);
+    [pks,locs] = findpeaks(z_matrix_no_g,'MINPEAKHEIGHT',minPeakHeight(indx));
+    numSteps(indx) = numel(pks);
+    indx = indx + 1;
+end
+
+figure
+plot(minPeakHeight, numSteps)
+title('Number of peaks Vs. minimum peak height');
+xlabel('Minimum peak height');
+ylabel('Number of peaks');
+grid on
 
 figure
 plot(time_matrix, z_matrix_no_g);
-title("Z acceleration without gravity vs Time, Number of steps = " + numSteps)
+title("Z acceleration without gravity vs Time, Number of steps = " + numSteps(13))
 xlabel('Time [s]');
 ylabel('Acceleration [m/s^2]');
 grid on
 hold on
+[pks,locs] = findpeaks(z_matrix_no_g,'MINPEAKHEIGHT',minPeakHeight(13));
 plot(time_matrix(locs), pks, 'r', 'Marker', 'v', 'LineStyle', 'none');
 
 % Acceleration Magnitude
